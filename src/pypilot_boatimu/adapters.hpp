8919f16735d6569f8700468899675af2c3741827
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-18 19:35:30 -0400

    Add BoatIMU adapter umbrella header

---
+++ src/pypilot_boatimu/adapters.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "pypilot_boatimu/adapters/generic_calibrated_sample_adapter.hpp"
+#include "pypilot_boatimu/adapters/generic_fused_attitude_adapter.hpp"
+#include "pypilot_boatimu/adapters/replay_adapter.hpp"
+#include "pypilot_boatimu/adapters/rtimulib2_adapter.hpp"
+#include "pypilot_boatimu/adapters/ocean_imu_adapter.hpp"
